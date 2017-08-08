6485c391fbcd092fd1c0ca09c79eabff0d9e9d19
AdamSimpson <adambsimpson@gmail.com>
2017-08-08 10:14:51 -0400

    Adding ability to read substitutions from file

--- src/dl-intercept.cpp
+++ src/dl-intercept.cpp
@@ -10,6 +10,7 @@
 #include <unordered_map>
 #include <sstream>
 #include <iostream>
+#include <fstream>
 #include "boost/algorithm/string.hpp"
 
 static std::unordered_map<std::string, std::string> substitutions;
@@ -42,24 +43,44 @@ static void process_environment_variables() {
   // Populate std::map with libraries and substitues in following form:
   // DL_INTERCEPT=original:substitute, original2:substitute2
   // e.g. DL_INTERCEPT="libmpi.so:/opt/cray/blarg/libmpi.so, libmpif.so:/opt/cray/blarg/libmpif.so"
-  const char *dl_cstring = std::getenv("DL_INTERCEPT");
+  // -OR-
+  // From a file with each library substituion pair on a new line
+  const char *dl_cstring = std::getenv("DL_SUBSTITUTIONS");
+
+  std::vector<std::string> substituion_pairs;
 
   if(dl_cstring != NULL) {
-    // Split DL_INTERCEPT substitution pairs on ","
-    std::vector<std::string> tokens;
-    boost::split(tokens, dl_cstring, boost::is_any_of(","));
+    std::string dl_substitutions(dl_cstring);
+
+    std::vector<std::string> substitution_pairs;
+
+    // Extract substitution pairs into vector
+    if(dl_substitutions.find(":") == std::string::npos) {
+      // Read substitution pairs line by line
+      std::ifstream substitutions_file;
+      substitutions_file.open(dl_substitutions, std::ifstream::in);
+      std::string line;
+      while(getline(substitutions_file, line)) {
+        substitution_pairs.push_back(line);
+      }
+      substitutions_file.close();
+    }
+    else {
+      // Split substitution pairs on ","
+      boost::split(substitution_pairs, dl_substitutions, boost::is_any_of(","));
+    }  
 
     // Split each substitution pair on ":"
-    for(std::string const& token : tokens) {
-      std::vector<std::string> split_token;
-      boost::split(split_token, token, boost::is_any_of(":"));
+    for(std::string const& pair : substitution_pairs) {
+      std::vector<std::string> split_pair;
+      boost::split(split_pair, pair, boost::is_any_of(":"));
 
       // Remove leading/trailing whitespace
-      boost::trim(split_token.front());
-      boost::trim(split_token.back());
+      boost::trim(split_pair.front());
+      boost::trim(split_pair.back());
 
       // Place substitutions into map
-      substitutions[split_token.front()] = split_token.back();
+      substitutions[split_pair.front()] = split_pair.back();
     }
   }
 }
